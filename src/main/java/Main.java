91885d0e2eef65cdc74002b251f08203d68fc60a
wers <pospelovandrej314@gmail.com>
2025-08-03 01:40:17 +0700

    one new line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git work check");
         System.out.println("New feature!");
+        System.out.println("Another branch!");
     }
 }
